db7af0c8247808babd7d2aee52990a08ff92e1ff
Levon Oganesyan <levon.oganesyan.am@gmail.com>
2022-08-20 00:38:32 +0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
 
 import { workerData } from 'worker_threads';
 import { SQS } from './aws-services/sqs';
-import { S3Bucket } from './aws-services/s3Bucket';
+import { S3Bucket } from './aws-services/s3-bucket';
 console.log(`Compilation passed successfully!`);
 const work = async () => {
 
 }
 
 const main = async () => {
-  // const mySqs = new SQS("MyNewSQS1");
-  // await mySqs.deploy();
+  const mySqs = new SQS("MyNewSQS1");
+  await mySqs.deploy();
   // await work();
   // await mySqs.undeploy();
 
   const myBucket = new S3Bucket("my-bucket-for-levon-arman");
   await myBucket.deploy();
-  await myBucket.undeploy();
+  // await myBucket.undeploy();
 }
 
 main()
