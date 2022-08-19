93936ec9a48ae3deb7b4325ac199770483c33bab
Levon Oganesyan <levon.oganesyan.am@gmail.com>
2022-08-20 03:21:33 +0400

    added source code archivation

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,20 +2,24 @@
 import { workerData } from 'worker_threads';
 import { SQS } from './aws-services/sqs';
 import { S3Bucket } from './aws-services/s3-bucket';
+import { urlToHttpOptions } from 'url';
+import { archiveSourceCode } from './utilities/common-utils'
 console.log(`Compilation passed successfully!`);
 const work = async () => {
 
 }
 
 const main = async () => {
-  const mySqs = new SQS("MyNewSQS1");
-  await mySqs.deploy();
+  // const mySqs = new SQS("MyNewSQS1");
+  // await mySqs.deploy();
   // await work();
   // await mySqs.undeploy();
 
-  const myBucket = new S3Bucket("my-bucket-for-levon-arman");
-  await myBucket.deploy();
+  // const myBucket = new S3Bucket("my-bucket-for-levon-arman");
+  // await myBucket.deploy();
   // await myBucket.undeploy();
+  
+  archiveSourceCode();
 }
 
 main()
--- server/src/utilities/common-utils.ts
+++ server/src/utilities/common-utils.ts
@@ -1,9 +1,10 @@
 
+import archiver = require("archiver");
+import * as fs from 'fs'
 
-
- export type NotNull<T> = T extends null ? never : T;
- export type NotUndefined<T> = T extends undefined ? never : T;
- export type NotNullNotUndefined<T> = NonNullable<T>;
+export type NotNull<T> = T extends null ? never : T;
+export type NotUndefined<T> = T extends undefined ? never : T;
+export type NotNullNotUndefined<T> = NonNullable<T>;
 
 export const isUndefined = (x: unknown): x is undefined => {
     return x === undefined;
@@ -27,4 +28,59 @@ export const throwIfUndefined = <T>(x: T, message: string): asserts x is NotUnde
 
 export const sleep = (ms: number) => {
     return new Promise( resolve => setTimeout(resolve, ms) );
+}
+
+export const getCurrentDateAsString = () => {
+    const date = new Date();
+    return `${date.getFullYear()}-${date.getMonth()}-${date.getDay()}-${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}-${date.getMilliseconds()}`;
+}
+
+export const archiveSourceCode = async () => {
+    const pathToSourceCode = `${__dirname}/../../../codebases`;
+    fs.mkdir(pathToSourceCode, (err) => {
+        if (err) {
+            return console.error(err);
+        }
+        console.log(`Directory ${pathToSourceCode} created successfully!`);
+    });
+    const zipName : string = `codebase-${getCurrentDateAsString()}.zip`;
+    console.log(`Creating zip file: ${pathToSourceCode}/${zipName}`);
+    const output = fs.createWriteStream(`${pathToSourceCode}/${zipName}`);
+    const archive = archiver('zip', {
+        zlib: { level: 9 } // Sets the compression level.
+    });
+
+    output.on('close', function() {
+        console.log(archive.pointer() + ' total bytes');
+        console.log('archiver has been finalized and the output file descriptor has closed.');
+    });
+
+    output.on('end', function() {
+    console.log('Data has been drained');
+    });
+
+    archive.on('warning', function(err) {
+    if (err.code === 'ENOENT') {
+        // log warning
+    } else {
+        // throw error
+        throw err;
+    }
+    });
+
+    archive.on('error', function(err) {
+        throw err;
+    });
+
+    archive.pipe(output);
+
+    console.log(`Adding package.json to the zip file: ${zipName}`);
+    archive.file('package.json', { name: 'package.json' });
+    console.log(`Adding src/node_modules to the zip file: ${zipName}`);
+    archive.directory('src/node_modules/', false);
+    console.log(`Adding src/dist to the zip file: ${zipName}`);
+    archive.directory('src/dist/', false);
+    await archive.finalize();
+    console.log(`Zip file ${zipName} created`);
+
 }
